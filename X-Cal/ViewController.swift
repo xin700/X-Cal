110c3c1de752ee21f4cd3b8dbc266b900614e2d7
xin <992950082@qq.com>
2023-09-30 22:21:06 +0800

    Add the actions to each button

--- X-Cal/ViewController.swift
+++ X-Cal/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var OutputNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func Zero(_ sender: Any) {
+    }
+    
+    @IBAction func One(_ sender: Any) {
+    }
+    @IBAction func Two(_ sender: Any) {
+    }
+    @IBAction func Three(_ sender: Any) {
+    }
+    @IBAction func Four(_ sender: Any) {
+    }
+    @IBAction func Five(_ sender: Any) {
+    }
+    @IBAction func Six(_ sender: Any) {
+    }
+    @IBAction func Seven(_ sender: Any) {
+    }
+    @IBAction func Eight(_ sender: Any) {
+    }
+    @IBAction func Nine(_ sender: Any) {
+    }
+    @IBAction func AC(_ sender: Any) {
+    }
+    @IBAction func Reverse(_ sender: Any) {
+    }
+    @IBAction func Mo(_ sender: Any) {
+    }
+    @IBAction func Div(_ sender: Any) {
+    }
+    @IBAction func Mul(_ sender: Any) {
+    }
+    @IBAction func Sub(_ sender: Any) {
+    }
+    @IBAction func Plus(_ sender: Any) {
+    }
+    @IBAction func Equal(_ sender: Any) {
+    }
+    @IBAction func Dot(_ sender: Any) {
+    }
 }
 
