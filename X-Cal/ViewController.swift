15fc812563bc6c468299c6c0480b45cf84f1db61
xin <992950082@qq.com>
2023-09-30 20:04:13 +0800

    Initial Commit

---
+++ X-Cal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  X-Cal
+//
+//  Created by xin on 2023/9/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ X-CalUITests/X_CalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  X_CalUITestsLaunchTests.swift
+//  X-CalUITests
+//
+//  Created by xin on 2023/9/30.
+//
+
+import XCTest
+
+final class X_CalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
